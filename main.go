861f76272b8d5e362bda1661397eb9b890a08c81
larrygf02 <raulgf_02@hotmail.com>
2021-04-17 19:53:16 -0500

    Add main

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/go-twitter/bd"
+	"github.com/go-twitter/handler"
 )
 
 func main() {
 	fmt.Println("Hola API Twitter")
+	if bd.CheckConnect() == 0 {
+		log.Fatal("Sin conexión a la BD")
+		return
+	}
+	handler.HandlerRouters()
 }
