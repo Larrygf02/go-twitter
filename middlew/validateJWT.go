a188ff0da2446b443f2c865893ecb3a26f1fd0f6
larrygf02 <raulgf_02@hotmail.com>
2021-04-21 18:14:00 -0500

    Add middleware token

---
+++ middlew/validateJWT.go
@@ -0,0 +1,14 @@
+package middlew
+
+import "net/http"
+
+func ValidateJWT(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, _, _, err := routes.ProcessToken(r.Header.Get("Authorization"))
+		if err != nil {
+			http.Error(w, "Error en el token"+err.Error(), 400)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
