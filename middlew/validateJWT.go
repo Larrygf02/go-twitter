80ea0e1a8d3529dc770c85e9cc25cb91db81b279
larrygf02 <raulgf_02@hotmail.com>
2021-04-21 18:37:00 -0500

    Process token

--- middlew/validateJWT.go
+++ middlew/validateJWT.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func ValidateJWT(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_, _, _, err := routes.ProcessToken(r.Header.Get("Authorization"))
+		_, _, _, err := routers.ProcessToken(r.Header.Get("Authorization"))
 		if err != nil {
 			http.Error(w, "Error en el token"+err.Error(), 400)
 			return
---
+++ routers/processToken.go
@@ -0,0 +1,39 @@
+package routers
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-twitter/bd"
+	"github.com/go-twitter/models"
+)
+
+var Email string
+var IdUser string
+
+func ProcessToken(token string) (*models.Claim, bool, string, error) {
+	key_code := []byte("GOLANGTWITTER")
+	claims := &models.Claim{}
+	splitToken := strings.Split(token, "Bearer")
+	if len(splitToken) != 2 {
+		return claims, false, string(""), errors.New("formato de token invalido")
+	}
+	token = strings.TrimSpace(splitToken[1])
+	tkn, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return key_code, nil
+	})
+	if err == nil {
+		_, found, _ := bd.ExistUser(claims.Email)
+		if found {
+			Email = claims.Email
+			IdUser = claims.ID.Hex()
+		}
+		return claims, found, IdUser, nil
+	}
+
+	if !tkn.Valid {
+		return claims, false, string(""), errors.New("Token invalido")
+	}
+	return claims, false, string(""), err
+}
