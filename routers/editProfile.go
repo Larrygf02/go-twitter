0add2a0e6c14cae72ea84b3cb48aa390ed6c84d0
larrygf02 <raulgf_02@hotmail.com>
2021-04-21 21:24:15 -0500

    Edit profile

---
+++ routers/editProfile.go
@@ -0,0 +1,32 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-twitter/bd"
+	"github.com/go-twitter/models"
+)
+
+func editProfile(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Datos incorrectos"+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var status bool
+	status, err = bd.EditUser(user, IdUser)
+	if err != nil {
+		http.Error(w, "Ocurrio un error al intentar el editar el usuario", http.StatusBadRequest)
+		return
+	}
+	if !status {
+		http.Error(w, "No se ha logrado modificar el registro del usuario", http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+
+}
