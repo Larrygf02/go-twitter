004770965f63ad5e57cefa8aabb0e531d85566cb
larrygf02 <raulgf_02@hotmail.com>
2021-04-21 23:56:35 -0500

    add route tweet

---
+++ routers/registerTweet.go
@@ -0,0 +1,37 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/go-twitter/bd"
+	"github.com/go-twitter/models"
+)
+
+func RegisterTweet(w http.ResponseWriter, r *http.Request) {
+	var tweet models.Tweet
+	err := json.NewDecoder(r.Body).Decode(&tweet)
+
+	if err != nil {
+		http.Error(w, "Ocurrio en el cuerpo del tweet", http.StatusBadRequest)
+		return
+	}
+
+	register := models.Tweet{
+		UserId:      IdUser,
+		Message:     tweet.Message,
+		CreatedDate: time.Now(),
+	}
+	_, status, err := bd.InsertTweet(register)
+	if err != nil {
+		http.Error(w, "Ocurrio un error al momento de grabar el tweet", http.StatusBadRequest)
+		return
+	}
+	if !status {
+		http.Error(w, "Ocurrio un error insesperado", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
