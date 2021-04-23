f96a7122980a80b689de1583f8bc60b4ac2a6a3c
larrygf02 <raulgf_02@hotmail.com>
2021-04-22 22:27:12 -0500

    Get banner

---
+++ routers/getBanner.go
@@ -0,0 +1,32 @@
+package routers
+
+import (
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/go-twitter/bd"
+)
+
+func GetBanner(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parametro ID", http.StatusBadRequest)
+		return
+	}
+
+	profile, err := bd.GetProfile(ID)
+	if err != nil {
+		http.Error(w, "Usuario no encontrado", http.StatusBadRequest)
+		return
+	}
+	openFile, err := os.Open("uploads/banners/" + profile.Avatar)
+	if err != nil {
+		http.Error(w, "Imagen no encontrada", http.StatusBadRequest)
+		return
+	}
+	_, err = io.Copy(w, openFile)
+	if err != nil {
+		http.Error(w, "Imagen al copiar", http.StatusBadRequest)
+	}
+}
