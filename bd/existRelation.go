3dfc5c8a11f6c1152b0e1d3415382e9b40b21c8c
larrygf02 <raulgf_02@hotmail.com>
2021-04-22 23:53:48 -0500

    Add existRelation

---
+++ bd/existRelation.go
@@ -0,0 +1,31 @@
+package bd
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-twitter/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func existRelation(relation models.Relation) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twitter")
+	collection := db.Collection("relacion")
+
+	condition := bson.M{
+		"userid":       relation.UserId,
+		"userrelation": relation.UserRelation,
+	}
+	var result models.Relation
+	fmt.Println(result)
+	err := collection.FindOne(ctx, condition).Decode(&result)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false, err
+	}
+	return true, nil
+}
