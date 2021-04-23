d1ebd2b9d696f188cfe6954e5ffd882fd7085719
larrygf02 <raulgf_02@hotmail.com>
2021-04-22 19:17:33 -0500

    get tweet router

---
+++ routers/getTweets.go
@@ -0,0 +1,37 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/go-twitter/bd"
+)
+
+func GetTweets(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parametro id", http.StatusBadRequest)
+		return
+	}
+	if len(r.URL.Query().Get("page")) < 1 {
+		http.Error(w, "Debe enviar el parametro pagina", http.StatusBadRequest)
+		return
+	}
+
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil {
+		http.Error(w, "Error en el numero de pagina", http.StatusBadRequest)
+		return
+	}
+	pag := int64(page)
+	response, status := bd.GetTweet(ID, pag)
+	if !status {
+		http.Error(w, "Error al leer los tweets", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(response)
+}
