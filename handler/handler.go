36815ab9a3e45f50ab6bd7f7b189a675f537a334
larrygf02 <raulgf_02@hotmail.com>
2021-04-23 23:08:33 -0500

    back

--- handler/handler.go
+++ handler/handler.go
@@ -28,7 +28,7 @@ func HandlerRouters() {
 	router.HandleFunc("/relation", middlew.CheckBD(middlew.ValidateJWT(routers.DeleteRelation))).Methods("DELETE")
 	router.HandleFunc("/relation", middlew.CheckBD(middlew.ValidateJWT(routers.ExistRelation))).Methods("GET")
 	router.HandleFunc("/list-user", middlew.CheckBD(middlew.ValidateJWT(routers.ViewUsers))).Methods("GET")
-	router.HandleFunc("/list-tweets", middlew.CheckBD(middlew.ValidateJWT(routers.ReadTweetsFollowers))).Methods("GET")
+	router.HandleFunc("/list-tweets", middlew.CheckBD(middlew.ValidateJWT(routers.ReadTweetsFollowers))).Methods("GET") // leer tweets
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "8081"
