169d87f2dececaf60fe92273981b48d40673cd96
larrygf02 <raulgf_02@hotmail.com>
2021-05-13 22:10:55 -0500

    Upload Avatar to s3

--- models/s3.go
+++ models/s3.go
@@ -2,7 +2,9 @@ package models
 
 import (
 	"fmt"
+	"log"
 	"mime/multipart"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -37,7 +39,6 @@ func (t *S3Client) Upload(file multipart.File, myBucket string, keyName string) 
 	uploader := s3manager.NewUploader(t.Session)
 	result, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(myBucket),
-		ACL:    aws.String("public-read"),
 		Key:    aws.String(keyName),
 		Body:   file,
 	})
@@ -47,4 +48,17 @@ func (t *S3Client) Upload(file multipart.File, myBucket string, keyName string) 
 	}
 	fmt.Println(result)
 	return true, nil
+}
+
+func (t *S3Client) GenerateUrl(myBucket string, keyName string) string {
+	req, _ := t.Svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(myBucket),
+		Key:    aws.String(keyName),
+	})
+	urlStr, err := req.Presign(15 * time.Minute)
+	if err != nil {
+		log.Println("Failed to sign request", err)
+	}
+	fmt.Println(urlStr)
+	return urlStr
 }
--- routers/UploadAvatarS3.go
+++ routers/UploadAvatarS3.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/go-twitter/bd"
 	"github.com/go-twitter/models"
 )
 
@@ -12,13 +13,25 @@ func UploadAvatarS3(w http.ResponseWriter, r *http.Request) {
 	var extension = strings.Split(handler.Filename, ".")[1]
 	var filelocation string = "images/avatars/" + IdUser + "." + extension
 	var s3 = models.S3
+	var bucket string = "apps-go-twittor"
 	s3 = new(models.S3Client)
 	s3.Region = "us-east-1"
 	s3.NewSession(s3.Region)
-	_, err := s3.Upload(file, "apps-go-twittor", filelocation)
+	_, err := s3.Upload(file, bucket, filelocation)
 	if err != nil {
 		http.Error(w, "Error al cargar la imagen!"+err.Error(), http.StatusBadRequest)
 		return
 	}
+	// Update Avatar
+	var user models.User
+	var status bool
+	user.Avatar = "https://" + bucket + ".s3.amazonaws.com/" + filelocation
+	status, err = bd.EditUser(user, IdUser)
+	if err != nil || !status {
+		http.Error(w, "Error al grabar el avatar en el user", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
 }
