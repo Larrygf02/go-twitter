d4306fb64744b802ecb0c41fc970294c12981884
larrygf02 <raulgf_02@hotmail.com>
2021-05-31 20:45:27 -0500

    Insert tweet comment

--- bd/tweetHandler.go
+++ bd/tweetHandler.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-twitter/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Obtener quienes comentaron el tweet
@@ -26,4 +27,32 @@ func GetCommentsTweet(ID string) ([]*models.Tweet, error) {
 		return results, err
 	}
 	return results, nil
+}
+
+// Insert comment in tweets
+
+func InsertCommentTweet(tweet models.Tweet) (string, bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	db := MongoCN.Database("twitter")
+	collection := db.Collection("tweet")
+
+	register := bson.M{
+		"userid":          tweet.UserId,
+		"message":         tweet.Message,
+		"date":            tweet.CreatedDate,
+		"is_comment":      tweet.IsComment,      // si el tweeter es un comentario
+		"twitter_comment": tweet.TwitterComment, // id de tweeter que esta comentando
+		"is_retweet":      tweet.IsRetweet,      // si el tweeter es un retweet
+		"twitter_retweet": tweet.TwitterRetweet,
+	}
+
+	result, err := collection.InsertOne(ctx, register)
+	if err != nil {
+		return string(""), false, err
+	}
+
+	objID, _ := result.InsertedID.(primitive.ObjectID)
+	return objID.String(), true, nil
 }
--- models/tweet.go
+++ models/tweet.go
@@ -7,14 +7,22 @@ import (
 )
 
 type Tweet struct {
-	UserId      string    `bson:"userid" json:"userid,omitempty"`
-	Message     string    `bson:"message" json:"message,omitempty"`
-	CreatedDate time.Time `bson:"date" json:"date,omitempty"`
+	UserId         string    `bson:"userid" json:"userid,omitempty"`
+	Message        string    `bson:"message" json:"message,omitempty"`
+	CreatedDate    time.Time `bson:"date" json:"date,omitempty"`
+	IsComment      bool      `bson:"is_comment" json:"is_comment, omitempty"`
+	TwitterComment string    `bson:"twitter_comment" json:"twitter_comment, omitempty"`
+	IsRetweet      bool      `bson:"is_retweet" json:"is_retweet, omitempty"`
+	TwitterRetweet string    `bson:"twitter_retweet" json:"twitter_retweet, omitempty"`
 }
 
 type GetTweet struct {
-	ID          primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
-	UserId      string             `bson:"userid" json:"userid,omitempty"`
-	Message     string             `bson:"message" json:"message,omitempty"`
-	CreatedDate time.Time          `bson:"date" json:"date,omitempty"`
+	ID             primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
+	UserId         string             `bson:"userid" json:"userid,omitempty"`
+	Message        string             `bson:"message" json:"message,omitempty"`
+	CreatedDate    time.Time          `bson:"date" json:"date,omitempty"`
+	IsComment      bool               `bson:"is_comment" json:"is_comment, omitempty"`
+	TwitterComment string             `bson:"twitter_comment" json:"twitter_comment, omitempty"`
+	IsRetweet      bool               `bson:"is_retweet" json:"is_retweet, omitempty"`
+	TwitterRetweet string             `bson:"twitter_retweet" json:"twitter_retweet, omitempty"`
 }
