777c2d2d95ddb7c0de8de69ca98a1fc65192b0d9
larrygf02 <raulgf_02@hotmail.com>
2021-06-19 22:36:34 -0500

    Redis singlenton

--- utils/redis.go
+++ utils/redis.go
@@ -2,12 +2,31 @@ package utils
 
 import (
 	"os"
+	"sync"
 
 	"github.com/go-redis/redis"
 )
 
+var lock = &sync.Mutex{}
+var client *redis.Client
+
 func GetClientRedis() redis.Client {
-	opt, _ := redis.ParseURL(os.Getenv("REDIS_URL"))
-	client := redis.NewClient(opt)
+	if client == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		var local = false
+		if local {
+			client = redis.NewClient(&redis.Options{
+				Addr:     "localhost:6379",
+				Password: "", // no password set
+				DB:       0,  // use default DB
+			})
+			return *client
+		} else {
+			opt, _ := redis.ParseURL(os.Getenv("REDIS_URL"))
+			client = redis.NewClient(opt)
+			return *client
+		}
+	}
 	return *client
 }
